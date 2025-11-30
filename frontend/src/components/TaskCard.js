9d396ffd2e7cabe8d366955b7300840588b738f3
Tashfia123 <tashfiahassan@iut-dhaka.edu>
2025-11-30 11:14:02 +0600

    mmm

--- frontend/src/components/TaskCard.js
+++ frontend/src/components/TaskCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './TaskCard.css';
 
 function TaskCard({ task, onUpdateTask, onDeleteTask, onDragStart, onDragEnd, isDragging, projectColorMap }) {
